c946aa7204f880720183a6f4259a5255bd308863
Melo <70630094+neonwhiskers@users.noreply.github.com>
2023-11-13 14:06:25 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,9 @@
 services.openssh.settings.PermitRootLogin = "yes";
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.forceInstall = true;
+
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
